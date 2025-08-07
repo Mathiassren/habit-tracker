744f1c6351166f95ec2ea7cf9a0a96b814956a49
Mathiassren <Mathiassrensen@yahoo.dk>
2025-08-07 22:48:18 +0200

    added manifest to meta

---
+++ habit-tracker/src/app/head.js
@@ -0,0 +1,13 @@
+// src/app/head.js
+export default function Head() {
+  return (
+    <>
+      <title>Habit Tracker</title>
+      <meta name="description" content="Track your habits easily" />
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="theme-color" content="#9333ea" />
+      <meta name="mobile-web-app-capable" content="yes" />
+      <link rel="apple-touch-icon" href="/icon-192.png" />
+    </>
+  );
+}
